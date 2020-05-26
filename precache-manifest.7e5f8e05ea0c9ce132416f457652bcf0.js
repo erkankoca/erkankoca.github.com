066acaf859772e09cefcf5ea0ed2cc516c5ac621
erkankoca <erkan.koca@bil.omu.edu.tr>
2020-05-26 15:55:06 +0300

    resume changes

---
+++ precache-manifest.7e5f8e05ea0c9ce132416f457652bcf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74922915fe5d0d10cc08cec326930713",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ce000ee2552a1823142",
+    "url": "/static/js/2.ba0ead68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba0ead68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ed9f39fbecde223ca9",
+    "url": "/static/js/main.160aa6d7.chunk.js"
+  },
+  {
+    "revision": "80e274ec0ec388e5e202",
+    "url": "/static/js/runtime-main.c8b434d9.js"
+  }
+]);
